41cb0942403f130ed9eb9390fffc80c7cfbdf012
Tony Seebregts <tony.seebregts@gmail.com>
2020-03-13 12:02:39 -0700

    Added GetCardByIndexN to device interface to support ACL

--- device/device.go
+++ device/device.go
@@ -1,5 +1,10 @@
-package uhppote
+package device
+
+import (
+	"github.com/uhppoted/uhppote-core/types"
+)
 
 type IDevice interface {
-	GetCardsN(serialNumber uint32) (uint32, error)
+	GetCardsN(deviceID uint32) (uint32, error)
+	GetCardByIndexN(deviceID, index uint32) (*types.Card, error)
 }
--- types/card.go
+++ types/card.go
@@ -27,6 +27,19 @@ func (c Card) String() string {
 	return fmt.Sprintf("%-8v %v %v %s %s %s %s", c.CardNumber, c.From, c.To, f(c.Doors[0]), f(c.Doors[1]), f(c.Doors[2]), f(c.Doors[3]))
 }
 
+func (c *Card) Clone() Card {
+	card := Card{
+		CardNumber: c.CardNumber,
+		From:       c.From,
+		To:         c.To,
+		Doors:      make([]bool, len(c.Doors)),
+	}
+
+	copy(card.Doors, c.Doors)
+
+	return card
+}
+
 func (r *Authorised) String() string {
 	return fmt.Sprintf("%v %v", r.SerialNumber, r.Authorised)
 }
