582e323e671e6debd8a78ed070f701c49ed6a4c9
Tony Seebregts <tony.seebregts@gmail.com>
2020-04-10 12:42:00 -0700

    Added 'GetCardByID' to IDevice interface to support 'grant' and 'revoke'

--- uhppote/get_card.go
+++ uhppote/get_card.go
@@ -104,7 +104,7 @@ func (u *UHPPOTE) GetCardByID(serialNumber, cardNumber uint32) (*types.Card, err
 	}
 
 	if uint32(response.SerialNumber) != serialNumber {
-		return nil, errors.New(fmt.Sprintf("Incorrect serial number in response - expect '%v', received '%v'", serialNumber, response.SerialNumber))
+		return nil, errors.New(fmt.Sprintf("Incorrect serial number in response - expected '%v', received '%v'", serialNumber, response.SerialNumber))
 	}
 
 	if response.CardNumber == 0 {
@@ -112,7 +112,51 @@ func (u *UHPPOTE) GetCardByID(serialNumber, cardNumber uint32) (*types.Card, err
 	}
 
 	if response.CardNumber != cardNumber {
-		return nil, errors.New(fmt.Sprintf("Incorrect card number in response - expect '%v', received '%v'", cardNumber, response.CardNumber))
+		return nil, errors.New(fmt.Sprintf("Incorrect card number in response - expected '%v', received '%v'", cardNumber, response.CardNumber))
+	}
+
+	if response.From == nil {
+		return nil, errors.New(fmt.Sprintf("Invalid 'from' date in response"))
+	}
+
+	if response.To == nil {
+		return nil, errors.New(fmt.Sprintf("Invalid 'to' date in response"))
+	}
+
+	return &types.Card{
+		CardNumber: response.CardNumber,
+		From:       *response.From,
+		To:         *response.To,
+		Doors:      []bool{response.Door1, response.Door2, response.Door3, response.Door4},
+	}, nil
+}
+
+func (u *UHPPOTE) GetCardByIDN(deviceID, cardID uint32) (*types.Card, error) {
+	request := messages.GetCardByIDRequest{
+		SerialNumber: types.SerialNumber(deviceID),
+		CardNumber:   cardID,
+	}
+
+	reply, err := u.Send(deviceID, request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, ok := reply.(*messages.GetCardByIDResponse)
+	if !ok {
+		return nil, errors.New("Invalid response to GetCardById")
+	}
+
+	if uint32(response.SerialNumber) != deviceID {
+		return nil, errors.New(fmt.Sprintf("Incorrect serial number in response - expected '%v', received '%v'", deviceID, response.SerialNumber))
+	}
+
+	if response.CardNumber == 0 {
+		return nil, nil
+	}
+
+	if response.CardNumber != cardID {
+		return nil, errors.New(fmt.Sprintf("Incorrect card number in response - expected '%v', received '%v'", cardID, response.CardNumber))
 	}
 
 	if response.From == nil {
