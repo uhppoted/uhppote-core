6831b1d870f6df6097bca96acb0530f34e6db7a2
twystd <twystd@users.noreply.github.com>
2023-01-16 11:52:39 -0800

    godoc 'Overview' skeleton (cf. https://github.com/uhppoted/uhppoted/issues/22)

---
+++ doc.go
@@ -0,0 +1,23 @@
+// Copyright 2009 uhppoted@twyst.co.za. All rights reserved.
+// Use of this source code is governed by an MIT-style license
+// that can be found in the LICENSE file.
+
+/*
+Package uhppote-core implements the public API for UHPPOTE TCP/IP Wiegand-26
+access controllers.
+
+The API comprises an RPC-like protocol implemented using
+64 byte UDP messages between the controller and the application.
+
+# Structure
+
+The library consists of the following packages:
+  - [uhppote-core/uhppote] which wraps the UDP request/response packet handling for the
+    communication with the the access controller.
+  - [uhppote-core/messages] which defines the UDP message structures used for the RPC-like
+    API.
+  - [uhppote-core/types] which defines the data types used in the `messages` package.
+  - [uhppote-core/encoding\UTO311-L0x] which implements message marshalling and unmarshalling
+    to and from the 64 byte packets exchangeed with the access controller.
+*/
+package core
