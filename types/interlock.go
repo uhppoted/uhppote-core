11f5ee4c0d834ef39a41002e4a37bb35c0c978ce
twystd <twystd@users.noreply.github.com>
2023-05-29 13:07:46 -0700

    Updated set-interlock to match actual controller behaviour (cf. https://github.com/uhppoted/uhppote-core/issues/14 and https://github.com/uhppoted/uhppoted-dll/issues/6)

--- types/interlock.go
+++ types/interlock.go
@@ -4,12 +4,28 @@ type Interlock uint8
 
 const (
 	NoInterlock    Interlock = 0x00 // disabled
-	Interlock12_34 Interlock = 0x01 // reader (1,2) and (3,4)
-	Interlock13_24 Interlock = 0x02 // reader (1,3) and (2,4)
-	Interlock123   Interlock = 0x03 // reader (1,2,3)
-	Interlock1234  Interlock = 0x04 // reader (1,2,3,4)
+	Interlock12    Interlock = 0x01 // doors (1,2)
+	Interlock34    Interlock = 0x02 // doors (3,4)
+	Interlock12_34 Interlock = 0x03 // doors (1,2) and (3,4)
+	Interlock123   Interlock = 0x04 // doors (1,2,3)
+	Interlock1234  Interlock = 0x08 // doors (1,2,3,4)
 )
 
 func (i Interlock) String() string {
-	return [...]string{"disabled", "1&2,3&4", "1&3,2&4", "1&2&3", "1&2&3&4"}[i]
+	switch i {
+	case NoInterlock:
+		return "disabled"
+	case Interlock12:
+		return "1&2"
+	case Interlock34:
+		return "3&4"
+	case Interlock12_34:
+		return "1&2,3&4"
+	case Interlock123:
+		return "1&2&3"
+	case Interlock1234:
+		return "1&2&3&4"
+	}
+
+	return ""
 }
